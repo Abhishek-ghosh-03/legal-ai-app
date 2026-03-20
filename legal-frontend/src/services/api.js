dd617cc7a88f7e1754bb955c84054185a1695bb4
Abhishek <ghoshabhishek099@gmail.com>
2026-03-20 20:40:08 +0530

    Fix CORS for Netlify

--- legal-frontend/src/services/api.js
+++ legal-frontend/src/services/api.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const API = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL
 });
-// 🔐 Attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
