03819e6630074348b6dd83c69979d0be53694976
Abhishek <ghoshabhishek099@gmail.com>
2026-03-20 19:49:17 +0530

    Updated CORS config for deployment

--- legal-simplifier-backend/src/app.js
+++ legal-simplifier-backend/src/app.js
@@ -5,7 +5,10 @@ import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/docs", documentRoutes);
