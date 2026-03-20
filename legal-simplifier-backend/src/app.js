84a5a5ee4e211d4f19183bb51c0a62ef89966794
Abhishek <ghoshabhishek099@gmail.com>
2026-03-20 20:25:01 +0530

    Fix CORS for production

--- legal-simplifier-backend/src/app.js
+++ legal-simplifier-backend/src/app.js
@@ -6,7 +6,7 @@ import authRoutes from "./routes/authRoutes.js";
 const app = express();
 
 app.use(cors({
-  origin: "*",
+  origin: "https://legal-aii.netlify.app",
   credentials: true
 }));
 app.use(express.json());
