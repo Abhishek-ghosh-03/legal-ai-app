f6daefaf0f684a375f9d7e8deab5422d34f91d64
Abhishek <ghoshabhishek099@gmail.com>
2026-03-20 20:29:39 +0530

    Fixed CORS and API route

--- legal-simplifier-backend/src/app.js
+++ legal-simplifier-backend/src/app.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(cors({
   origin: "https://legal-aii.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
 app.use(express.json());
