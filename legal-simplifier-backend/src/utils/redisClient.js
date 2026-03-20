d3b629672f9ba2ee26586d79c097bb168dca1ef8
Abhishek <ghoshabhishek099@gmail.com>
2026-03-20 20:10:29 +0530

    Handled Redis for production

--- legal-simplifier-backend/src/utils/redisClient.js
+++ legal-simplifier-backend/src/utils/redisClient.js
@@ -1,25 +1,15 @@
 import Redis from "ioredis";
 
-export const redis = new Redis({
-  host: "127.0.0.1",
-  port: 6379,
+let redis;
 
+if (process.env.REDIS_URL) {
+  redis = new Redis(process.env.REDIS_URL);
+} else {
+  console.log("⚠️ Redis not configured, skipping...");
+  redis = {
+    get: async () => null,
+    set: async () => null
+  };
+}
 
-  retryStrategy(times) {
-    return Math.min(times * 50, 2000);
-  },
-
-
-  lazyConnect: true,
-});
-
-
-redis.connect();
-
-redis.on("connect", () => {
-  console.log("Redis connected ");
-});
-
-redis.on("error", (err) => {
-  console.error("Redis error:", err.message);
-})
+export { redis };
